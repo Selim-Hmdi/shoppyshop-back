92c8b86a67850c9d71f8f2d50956c643f9235fd4
Selim Hoummadi <selim_hoummadi@hotmail.com>
2022-06-13 12:08:08 +0200

    Initial commit

---
+++ src/main/java/com/groupe8/ShoppyShop/ShoppyShopApplication.java
@@ -0,0 +1,13 @@
+package com.groupe8.ShoppyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe8/ShoppyShop/ShoppyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe8.ShoppyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
