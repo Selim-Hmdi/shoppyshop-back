163f57be485998f1c253475ea7d769e9e8373de2
Camille <hermelcamille@gmail.com>
2022-06-16 09:19:18 +0200

    method to update and delete articles + to update user

--- src/main/java/com/groupe8/ShoppyShop/controller/ArticleController.java
+++ src/main/java/com/groupe8/ShoppyShop/controller/ArticleController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +50,17 @@ public class ArticleController {
 	@PostMapping
 	public Article create(@RequestBody Article article) {
 		return service.save(article);
+	}
+	
+	@PutMapping("/{id}")
+	public Article update(@RequestBody Article article) {
+		service.save(article);
+		return service.findById(article.getId());
+	}
+	
+	@DeleteMapping("/{id}")
+	public List<Article> delete(@PathVariable(name="id") int id) {
+		service.delete(id);
+		return service.findAll();
 	}
 }
